86f43f4ff4da3821c5da7e971485c0e897410c65
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-12-15 08:18:23 -0300

    added avatar uploader

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  def store_dir
+    "uploads/#{current_user.id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
+
+  def default_url(*_args)
+    'https://cookiecode-restaurant_manager.s3.us-east-2.amazonaws.com/defaults/default.jpg'
+  end
+
+  def asset_host
+    'http://localhost:3000/api'
+  end
+end
