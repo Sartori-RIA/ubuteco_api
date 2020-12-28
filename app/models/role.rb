2fb3d710d70f7baff056a4678abdd04b4c881e13
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-12-28 08:30:50 -0300

    ajustes specs

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  #validates :name, presence: true, uniqueness: true
 
   has_many :users, dependent: :restrict_with_error
 end
--- app/models/wine_style.rb
+++ app/models/wine_style.rb
@@ -3,7 +3,8 @@
 class WineStyle < ApplicationRecord
   acts_as_paranoid
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  #validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :wines, dependent: :restrict_with_error
 end
