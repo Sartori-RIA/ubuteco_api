3937762ea780733e59d8f786bd24047489f862a4
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-02-15 13:34:49 -0300

    fix sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,12 +1,14 @@
-Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = [:active_support_logger]
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:active_support_logger]
 
-  # To activate performance monitoring, set one of these options.
-  # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
-  # or
-  #config.traces_sampler = lambda do |context|
-  #    true
-  #end
+    # To activate performance monitoring, set one of these options.
+    # We recommend adjusting the value in production:
+    config.traces_sample_rate = 0.5
+    # or
+    #config.traces_sampler = lambda do |context|
+    #    true
+    #end
+  end
 end
