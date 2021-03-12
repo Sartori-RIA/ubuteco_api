1a8dd734213942219b7a9e8c49545c8e1a8df8dc
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-03-12 08:24:48 -0300

    added simplecov groups

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,4 +1,7 @@
 if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
-  SimpleCov.start :rails
+  SimpleCov.start :rails do
+    add_group "Uploaders", "app/uploaders"
+    add_group "Abilities", "app/models/abilities"
+  end
 end
