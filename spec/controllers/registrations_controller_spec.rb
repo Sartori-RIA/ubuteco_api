ba38ce738919cdc18198606a5a794171c698de0a
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-03-15 07:23:51 -0300

    fix specs

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -44,10 +44,6 @@ RSpec.describe RegistrationsController, type: :request do
       post user_registration_path, params: data.to_json, headers: unauthenticated_header
       expect(response).to have_http_status(:ok)
     end
-    it 'without params , raise ParameterMissing' do
-      params = { user: attributes_for(:user), organization_attributes: {} }
-      expect { post user_registration_path, params: params.to_json, headers: unauthenticated_header }.to raise_error ActionController::ParameterMissing, 'param is missing or the value is empty: organization_attributes'
-    end
     it 'without params bad_request' do
       post user_registration_path, params: { user: { name: "asd" } }.to_json, headers: unauthenticated_header
       expect(response).to have_http_status(:unprocessable_entity)
