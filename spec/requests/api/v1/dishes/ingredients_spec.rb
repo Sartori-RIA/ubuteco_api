6367ccdc3c1078e2468249d224f0983f46b37b48
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-04-03 08:41:55 -0300

    ingredients docs

--- spec/requests/api/v1/dishes/ingredients_spec.rb
+++ spec/requests/api/v1/dishes/ingredients_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Api::V1::Dishes::IngredientsController, type: :request do
       consumes 'application/json'
       security [bearerAuth: []]
       parameter name: :dish_id, in: :path, type: :string
+      response '200', 'Ok' do
+        let(:Authorization) { "Bearer #{auth_header(user)}" }
+        run_test!
+      end
       response '401', 'Unauthorized' do
         run_test!
       end
@@ -24,6 +28,27 @@ RSpec.describe Api::V1::Dishes::IngredientsController, type: :request do
       consumes 'application/json'
       security [bearerAuth: []]
       parameter name: :dish_id, in: :path, type: :string
+      parameter in: :body, type: :object,
+                schema: {
+                  properties: {
+                    id: { type: :integer },
+                    quantity: { type: :string },
+                    food_id: { type: :integer }
+                  },
+                  required: %w[quantity food_id]
+                }
+      response '201', 'Created' do
+        let(:Authorization) { "Bearer #{auth_header(user)}" }
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 food_id: { type: :integer },
+                 dish_id: { type: :integer },
+                 quantity: { type: :integer },
+               },
+               required: %w[id food_id food dish_id dish quantity]
+        run_test!
+      end
       response '401', 'Unauthorized' do
         run_test!
       end
@@ -41,13 +66,32 @@ RSpec.describe Api::V1::Dishes::IngredientsController, type: :request do
       end
     end
   end
-  path '/api/v1/dishes/{dish_id}/ingredients/{food_id}' do
+  path '/api/v1/dishes/{dish_id}/ingredients/{id}' do
     put 'Update ingredient in dish' do
       tags 'Dish ingredients'
       security [bearerAuth: []]
       consumes 'application/json'
       parameter name: :dish_id, in: :path, type: :string
-      parameter name: :food_id, in: :path, type: :string
+      parameter name: :id, in: :path, type: :string
+      parameter in: :body, type: :object,
+                schema: {
+                  properties: {
+                    quantity: { type: :string },
+                  },
+                  required: %w[quantity]
+                }
+      response '200', 'Ok' do
+        let(:Authorization) { "Bearer #{auth_header(user)}" }
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 food_id: { type: :integer },
+                 dish_id: { type: :integer },
+                 quantity: { type: :integer },
+               },
+               required: %w[id food_id food dish_id dish quantity]
+        run_test!
+      end
       response '401', 'Unauthorized' do
         run_test!
       end
@@ -69,7 +113,7 @@ RSpec.describe Api::V1::Dishes::IngredientsController, type: :request do
       security [bearerAuth: []]
       consumes 'application/json'
       parameter name: :dish_id, in: :path, type: :string
-      parameter name: :food_id, in: :path, type: :string
+      parameter name: :id, in: :path, type: :string
       response '401', 'Unauthorized' do
         run_test!
       end
